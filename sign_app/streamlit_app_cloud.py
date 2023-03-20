a11a80d0017da60ddc56fdcc362dccd9e51db13c
Georgian Tanaselea <30494267+georgetanaselea@users.noreply.github.com>
2023-03-20 11:54:57 +0000

    Update streamlit_app_cloud.py

--- sign_app/streamlit_app_cloud.py
+++ sign_app/streamlit_app_cloud.py
@@ -24,16 +24,16 @@ config.run_functions_eagerly(True)
 option = " "
 
 RTC_CONFIGURATION = RTCConfiguration(
-    {"iceServers":[{"urls": "stun:relay.metered.ca:80"},
-                                 {"urls": "turn:relay.metered.ca:80",
-                                  "username": "fde6cbb36d18c153785bf733",
-                                  "credential": "bEuNDxnIvdtIPPvB"},
-                                 {"urls": "turn:relay.metered.ca:443",
-                                  "username": "fde6cbb36d18c153785bf733",
-                                  "credential": "bEuNDxnIvdtIPPvB"},
-                                 {"urls": "turn:relay.metered.ca:443?transport=tcp",
-                                  "username": "fde6cbb36d18c153785bf733",
-                                  "credential": "bEuNDxnIvdtIPPvB"}]}
+    {"iceServers":[{"urls": ["stun:eu-turn5.xirsys.com"]},
+               {"username": "wvyohM6YQLnnWHVbjC57QCyuSje499sygAgHX1l5E3pRz7Fo-Nha1Uo439Mu-ZGnAAAAAGQYRW1nZW9yZ2lhbnRhbmFzZWxlYQ==",
+                "credential": "91a267b8-c713-11ed-9a20-0242ac140004",
+                "urls": ["turn:eu-turn5.xirsys.com:80?transport=udp",
+                         "turn:eu-turn5.xirsys.com:3478?transport=udp",
+                         "turn:eu-turn5.xirsys.com:80?transport=tcp",
+                         "turn:eu-turn5.xirsys.com:3478?transport=tcp",
+                         "turns:eu-turn5.xirsys.com:443?transport=tcp",
+                         "turns:eu-turn5.xirsys.com:5349?transport=tcp"]}]}
+
 
 )
 list_of_predictions = []
