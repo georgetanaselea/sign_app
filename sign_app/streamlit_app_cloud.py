9add816c448dbb51a174a8577cfa23cf6b9b9b31
Georgian Tanaselea <30494267+georgetanaselea@users.noreply.github.com>
2023-03-20 12:20:04 +0000

    Update streamlit_app_cloud.py

--- sign_app/streamlit_app_cloud.py
+++ sign_app/streamlit_app_cloud.py
@@ -24,7 +24,15 @@ config.run_functions_eagerly(True)
 option = " "
 
 RTC_CONFIGURATION = RTCConfiguration(
-    {"iceServers":[{"urls": ["stun:eu-turn5.xirsys.com"]}]}
+    {"iceServers":[ice_servers = [{"urls": ["stun:eu-turn5.xirsys.com"]},
+               {"username": "wvyohM6YQLnnWHVbjC57QCyuSje499sygAgHX1l5E3pRz7Fo-Nha1Uo439Mu-ZGnAAAAAGQYRW1nZW9yZ2lhbnRhbmFzZWxlYQ==",
+                "credential": "91a267b8-c713-11ed-9a20-0242ac140004",
+                "urls": ["turn:eu-turn5.xirsys.com:80?transport=udp",
+                         "turn:eu-turn5.xirsys.com:3478?transport=udp",
+                         "turn:eu-turn5.xirsys.com:80?transport=tcp",
+                         "turn:eu-turn5.xirsys.com:3478?transport=tcp",
+                         "turns:eu-turn5.xirsys.com:443?transport=tcp",
+                         "turns:eu-turn5.xirsys.com:5349?transport=tcp"]}]}
 
 )
 list_of_predictions = []
